fc5876034b334150c822a262c2e3b6fa084fd6bf
DylanF <dylanbrad10@gmail.com>
2023-02-09 19:38:26 -0500

    Fully debugged, writing readme now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 $(document).ready(function () {
   displayDate();
   initStorage();
@@ -9,11 +5,6 @@ $(document).ready(function () {
   $('.saveBtn').click(saveAppt);
 });
 
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
 function saveAppt() {
   var timeBlock = $(this).closest('.time-block');
   var timeBlockId = timeBlock.attr('id');
@@ -24,9 +15,6 @@ function saveAppt() {
   localStorage.setItem(timeBlockId, descBox);
 }
 
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
 function loadAppts() {
   for(let i=9; i <= 17; i++){
     let apptText = localStorage.getItem(`hour-${i}`);
@@ -43,7 +31,6 @@ function initStorage() {
   }
 }
 
-// TODO: Add code to display the current date in the header of the page.
 function displayDate() {
   var time = dayjs().format('dddd, MMMM DD, YYYY');
   var currentHour = dayjs().hour();
@@ -61,9 +48,3 @@ function displayDate() {
     }
   }
 }
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
